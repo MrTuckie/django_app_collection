0139cc3118aba1363e44bdce10e288182c1f6472
Arthur Lorencini <arthur@lorencini.tech>
2023-12-27 22:46:32 -0300

    feat: primeira parte do bin2dec funcionando adicionando .cpanel.yml testando o namecheap

--- bin2dec/views.py
+++ bin2dec/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the bin2dec index.")
+    print('fui chamado')
+    if request.method == "POST":
+        print("chamado dentro do post")
+        binary = request.POST["binary"]
+        decimal = 0
+        for index, digit in enumerate(reversed(binary)):
+            decimal += int(digit) * (2 ** index)
+        print(decimal)
+        return render(request, "bin2dec/index.html", {
+            "binary": binary,
+            "decimal": decimal,
+        })
+    else:
+        return render(request, "bin2dec/index.html")
---
+++ readme.md
@@ -0,0 +1,2 @@
+# lista de aplicativos da lorencini.tech
+O objetivo: https://github.com/florinpop17/app-ideas/tree/master
