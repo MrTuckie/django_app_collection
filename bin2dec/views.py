65149b3d5cbd7ab34d9133e6675220d62e63a850
Arthur Lorencini <arthur@lorencini.tech>
2024-01-03 18:52:02 -0300

    testando um lance aqui

--- bin2dec/views.py
+++ bin2dec/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print('fui chamado')
     if request.method == "POST":
-        print("chamado dentro do post")
+        
+        if "binary" not in request.POST:
+            return render(request, "bin2dec/index.html", {
+                "error": "Por favor, insira um número binário válido."
+            })
         binary = request.POST["binary"]
+        if len(binary) == 0:
+            return render(request, "bin2dec/index.html", {
+                "error": "Por favor, insira um número binário válido."
+            })
+        if len(binary) == 0 or binary[0].isdigit() == False or is_binary(binary) == False:
+            return render(request, "bin2dec/index.html", {
+                "error": "Por favor, insira um número binário válido."
+            })
         decimal = 0
         for index, digit in enumerate(reversed(binary)):
             decimal += int(digit) * (2 ** index)
-        print(decimal)
         return render(request, "bin2dec/index.html", {
             "binary": binary,
             "decimal": decimal,
         })
     else:
         return render(request, "bin2dec/index.html")
+    
+def is_binary(s):
+    return all(char in '01' for char in s)
