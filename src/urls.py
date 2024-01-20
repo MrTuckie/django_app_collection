92b39ec330dc0eabe8ebc42ee1557fa824ad3d21
Arthur Lorencini <arthur@lorencini.tech>
2024-01-20 09:32:26 -0300

    adding urls

--- src/urls.py
+++ src/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include("home.urls")),
     path('admin/', admin.site.urls),
     path("bin2dec/", include("bin2dec.urls"),name="bin2dec"),
     path("api/", include("api.urls"), name="api"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
