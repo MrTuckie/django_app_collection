d4bdc4b6a39e77e435cefd07d857860d113cb28a
Arthur Lorencini <arthur@lorencini.tech>
2024-01-24 20:56:51 -0300

    saci

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+class Test_home(TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.client = Client()
+
+    def test_home_view(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'home/index.html')
